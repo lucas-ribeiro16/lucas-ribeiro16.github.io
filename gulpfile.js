6ef51801ed6e8ad34f266d0748353aee4362cedc
lucas-ribeiro16 <lucaspereiraribeiro2001@gmail.com>
2017-03-27 18:03:34 -0300

    sjkdchuajzfhljkh

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,26 @@
 var gulp = require('gulp');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
-var concatCss = require('gulp-concat-css');
+var imagemin = require('gulp-imagemin');
 
 
 gulp.task('lucas', function(){
   console.log("oi eu sou o lucas")
 });
 
-gulp.task('default', function () {
+gulp.task('css', function () {
     gulp.src('src/css/**/*.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('concat', function () {
-  return gulp.src('src/css/**/*.css')
-    .pipe(concatCss("styles/bundle.css"))
-    .pipe(gulp.dest('out/'));
+gulp.task('default', function() {
+    gulp.watch('src/css/**/*.css', ['css','image']);
 });
+ 
+gulp.task('image', () =>
+    gulp.src('imagens/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/imagens'))
+);
